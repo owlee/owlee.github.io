0095522e0e5bb23b5c6cf4ef0441c5f784e7f367
Oscar Lee <owlee@umass.edu>
2017-02-08 00:14:31 -0500

    rough draft

--- js/contact_me.js
+++ js/contact_me.js
@@ -9,20 +9,20 @@ $(function() {
      submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
        // get values from FORM
-       var name = $("input#name").val();  
-       var email = $("input#email").val(); 
+       var name = $("input#name").val();
+       var email = $("input#email").val();
        var message = $("textarea#message").val();
         var firstName = name; // For Success/Failure Message
            // Check for white space in name for Success/Fail message
         if (firstName.indexOf(' ') >= 0) {
 	   firstName = name.split(' ').slice(0, -1).join(' ');
-         }        
+         }
 	 $.ajax({
                 url: "mail/contact_me.php",
             	type: "POST",
             	data: {name: name, email: email, message: message},
             	cache: false,
-            	success: function() {  
+            	success: function() {
             	// Success message
             	   $('#success').html("<div class='alert alert-success'>");
             	   $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -31,16 +31,16 @@ $(function() {
             		.append("<strong>Your message has been sent. </strong>");
  		  $('#success > .alert-success')
  			.append('</div>');
- 						    
+
  		  //clear all fields
  		  $('#contactForm').trigger("reset");
  	      },
- 	   error: function() {		
+ 	   error: function() {
  		// Fail message
  		 $('#success').html("<div class='alert alert-danger'>");
             	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             	 .append( "</button>");
-            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com'>me@example.com</a> ? Sorry for the inconvenience!");
+            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+", the mail server is down for maintenance at the moment.</strong> Could you please email Oscar directly to <a href='mailto:oscarwinglee@gmail.com?Subject=Message_Me from myprogrammingblog.com'>oscarwinglee@gmail.com</a>? So sorry for the inconvenience, and this issue will be resolved promptly!");
  	        $('#success > .alert-danger').append('</div>');
  		//clear all fields
  		$('#contactForm').trigger("reset");
@@ -57,9 +57,9 @@ $(function() {
                     $(this).tab("show");
         });
   });
- 
 
-/*When clicking on Full hide fail/success boxes */ 
+
+/*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
      $('#success').html('');
   });
